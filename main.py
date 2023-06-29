d89cfdcca6482bbdc63c12ac861df01df86f3440
cnst01 <cassionst@gmail.com>
2023-06-29 15:49:20 -0300

    path algorithm almost ready, just needs the robot modules, showing graphs with some problams

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from walls import check_walls
 import time
+import plotly.express as px
 
 class Position:
     def __init__(self, pos, views):
@@ -11,9 +12,13 @@ intersections = list()
 myMap = list()
 myMap.append(Position([0,0],check_walls()))
 
+class positions:  
+    x = list()
+    y = list()
 
-while True:
-    time.sleep(1)
+
+while len(myMap) - 50:
+    time.sleep(0.1)
     pose = myMap[-1] # pega o obj da posição atual
     msg = str(pose.pos) + "  " + str(pose.views)
     print(msg)
@@ -28,8 +33,10 @@ while True:
         else:
             intersections.append(pose) #grava nova interseccao
             if not pose.views[2]: #checa se ha caminho a direita
+                myMap[-1].views[2] == True #elimina o caminho da direita
                 myMap.append(Position([pose.pos[0]+1,pose.pos[1]],check_walls())) #cria nova posicao para direita
             else:
+                myMap[-1].views[1] == True #elimina o caminho do meio
                 myMap.append(Position([pose.pos[0],pose.pos[1]+1],check_walls())) #cria nova posicao para cima
     elif sum(pose.views) == 2: #checa se há caminho
         if not pose.views[2]:
@@ -50,9 +57,10 @@ while True:
             i-= 1
         print("voltei")
 
-     
-
-    
-
-    
-    
+for points in myMap:
+    positions.x.append(points.pos[0])
+    positions.y.append(points.pos[1])
+print(len(positions.x))
+print(len(positions.y))
+fig=px.scatter(x=positions.x, y=positions.y)
+fig.show()
