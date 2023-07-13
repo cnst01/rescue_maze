1eff194c5701c947e1f4441d264bfcf45658cb23
cnst01 <cassionst@gmail.com>
2023-07-13 17:21:45 -0300

    including moves like way to comeback

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ myMap.append(Position([0,0],check_walls()))
 
 myMoves = list()
 
+def create_pos(pos):
+   new_pos = goto(pose.pos, pos)
+   myMoves.append(new_pos.move)
+   myMap.append(Position(new_pos.pos,check_walls()))    
 
 while len(myMap) - 50:
     time.sleep(0.1)
@@ -21,8 +25,7 @@ while len(myMap) - 50:
     msg = str(pose.pos) + "  " + str(pose.views)
     # print(msg)
     if sum(pose.views) == 0: #checa se nao existe nenhuma parede
-        new_pos = goto(pose.pos, [pose.pos[0],pose.pos[1]+1]).pos
-        myMap.append(Position(new_pos,check_walls())) #adiciona uma posição acima ao mapa
+        create_pos([pose.pos[0],pose.pos[1]+1])#adiciona uma posição acima ao mapa
     elif sum(pose.views) == 1: #checa se é interseccao
         if pose.pos in intersections: #checa se já foi contabilizada
             if not myMap[-1].views[2]: #checa se havia o caminho da direita
@@ -33,35 +36,33 @@ while len(myMap) - 50:
             intersections.append(pose) #grava nova interseccao
             if not pose.views[2]: #checa se ha caminho a direita
                 myMap[-1].views[2] == True #elimina o caminho da direita
-                new_pos = goto(pose.pos,[pose.pos[0]+1,pose.pos[1]]).pos
-                myMap.append(Position(new_pos,check_walls())) #cria nova posicao para direita
+                create_pos([pose.pos[0]+1,pose.pos[1]])#cria nova posicao para direita
             else:
                 myMap[-1].views[1] == True #elimina o caminho do meio
-                new_pos = goto(pose.pos,[pose.pos[0],pose.pos[1]+1]).pos
-                myMap.append(Position(new_pos,check_walls())) #cria nova posicao para cima
+                create_pos([pose.pos[0],pose.pos[1]+1]) #cria nova posicao para cima
     elif sum(pose.views) == 2: #checa se há caminho
         if not pose.views[2]:
-            new_pos = goto(pose.pos,[pose.pos[0]+1,pose.pos[1]]).pos
-            myMap.append(Position(new_pos,check_walls())) #cria nova posicao para direita
+            create_pos([pose.pos[0]+1,pose.pos[1]]) #cria nova posicao para direita
         elif not pose.views[1]:
-            new_pos = goto(pose.pos,[pose.pos[0],pose.pos[1]+1]).pos
-            myMap.append(Position(new_pos,check_walls())) #cria nova posicao para cima
+            create_pos([pose.pos[0],pose.pos[1]+1])#cria nova posicao para cima
         else:
-            new_pos = goto(pose.pos,[pose.pos[0]-1,pose.pos[1]]).pos
-            myMap.append(Position(new_pos,check_walls())) #cria nova posicao para esquerda
+            create_pos([pose.pos[0]-1,pose.pos[1]]) #cria nova posicao para esquerda
     else:
         last_intersection = myMap.index(intersections[-1]) #recebe ultima interseccao
         i = len(myMap)-1 
         msg = str(myMap[-1].pos) + "  Sem saida\n\n"
-        # print(msg)
+        print(msg)
         while i > last_intersection:
-            goto(myMap[-1].pos,myMap[-2].pos).pos
+            # goto(myMap[-1].pos,myMap[-2].pos)
+            print(myMoves[-1])
+            myMoves.pop()
             myMap.pop() #elimina todas as posicoes ate ela
             msg = str(myMap[-1].pos) + "  " + str(myMap[-1].views)
             # print(msg)
             
             i-= 1
-        print("voltei")
+        msg = str(myMap[-1].pos) + "  voltei\n\n"
+        print(msg)
 
 def print_map():
 
